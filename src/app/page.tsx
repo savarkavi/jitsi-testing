10d6f7e5ac0625f06dd55df30012c47d124060d1
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-06-29 18:38:40 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,6 +117,7 @@ export default function Home() {
           }}
           userInfo={{
             displayName: displayName,
+            email: "",
           }}
           onApiReady={handleApiReady}
           getIFrameRef={(iframeRef) => {
